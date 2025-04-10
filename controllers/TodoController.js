a7d8c2b1c2fae5de5eef52dbf8abba48c99b00db
NaufalArkaan <naufalarkaan25@gmail.com>
2025-04-10 19:52:59 +0700

    Express.Js Last

--- controllers/TodoController.js
+++ controllers/TodoController.js
@@ -44,6 +44,47 @@ class TodoController {
         })
         console.log(req.params)
     }
+    static deleteTodo(req, res){
+        let id = +req.params.id
+
+        todo.destroy({
+            where:{
+                id
+            }
+        })
+        .then(result => {
+            if(result === 1){
+                res.json({
+                    massage: "Todo has been deleted!"
+                })
+            }else{
+                res.json({
+                    massage: "Todo failed to delete!"
+                })
+            }
+        })
+        .catch(err => {
+            res.json(err)
+        })
+    }
+    static updateTodo(req, res){
+        let id = +req.params.id
+        const{task, status} = req.body
+
+        todo.update({
+            task, status : Boolean(status)
+        },{
+            where: {
+                id
+            }
+        })
+        .then(result => {
+            res.json(result)
+        })
+        .catch(err => {
+            res.json(err)
+        })
+    }
 }
 
 module.exports = TodoController
--- routes/todo.js
+++ routes/todo.js
@@ -7,4 +7,8 @@ todoRoute.post('/add', TodoController.addTodos)
 
 todoRoute.get('/find/:id', TodoController.findById)
 
+todoRoute.delete('/delete/:id', TodoController.deleteTodo)
+
+todoRoute.put('/update/:id', TodoController.updateTodo)
+
 module.exports = todoRoute
